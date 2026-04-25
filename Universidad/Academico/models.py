86f05e927b2f6593c9c82bf964b85c2fb1e38c52
Martin Ramirez <comredsystem@gmail.com>
2026-04-25 12:53:46 -0500

    fist commit

---
+++ Universidad/Academico/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Curso(models.Model):
+    codigo = models.CharField(max_length=6)
+    nombre = models.CharField(max_length=30)
+    creditos = models.PositiveBigIntegerField()
+    
+    
